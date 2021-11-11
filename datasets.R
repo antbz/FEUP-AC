085c3ccb7c73199d7850b18c9fcbac7258f546f7
pedrojfs17 <up201806227@fe.up.pt>
2021-11-11 19:15:04 +0000

    Prepare datasets. ROC and AUC measures

--- datasets.R
+++ datasets.R
@@ -1,4 +1,5 @@
-# Data sets
+### Data Sets
+
 # account <- read.csv("data/account.csv", sep = ";")
 # card.test <- read.csv("data/card_test.csv", sep = ";")
 # card.train <- read.csv("data/card_train.csv", sep = ";")
@@ -10,18 +11,109 @@
 # transactions.test <- read.csv("data/trans_test.csv", sep = ";")
 # transactions.train <- read.csv("data/trans_train.csv", sep = ";")
 
-get.train.dataset <- function() {
-  loan.train <- read.csv("data/loan_train.csv", sep = ";")
-  account <- read.csv("data/account.csv", sep = ";")
-  disposition <- read.csv("data/disp.csv", sep = ";")
 
-  data <- loan.train %>%
+### Prepare Data Sets
+
+prepare.datasets <- function(train = TRUE) {
+  account.dataset()
+  client.dataset()
+  disposition.dataset()
+  demograph.dataset()
+
+  if (train) {
+    transacions.dataset(train = TRUE)
+    loan.dataset(train = TRUE)
+    card.dataset(train = TRUE)
+  } else {
+    transacions.dataset(train = FALSE)
+    loan.dataset(train = FALSE)
+    card.dataset(train = FALSE)
+  }
+}
+
+account.dataset <- function() {
+  account <- read.csv("data/account.csv", sep = ";")
+  account <<- account %>%
+    mutate(account_year = date %/% 10000 + 1900) %>%
+    mutate(account_month = date %% 10000 %/% 100) %>%
+    mutate(account_day = date %% 100) %>%
+    dplyr::select(-date)
+}
+
+client.dataset <- function() {
+  client <- read.csv("data/client.csv", sep = ";")
+  client <<- client %>%
+    mutate(birth_year = birth_number %/% 10000 + 1900) %>%
+    mutate(birth_month = birth_number %% 10000 %/% 100) %>%
+    mutate(birth_day = birth_number %% 100) %>%
+    mutate(gender = as.factor(ifelse(birth_month > 12, "women", "men"))) %>%
+    mutate(birth_month = ifelse(birth_month > 12, birth_month - 50, birth_month)) %>%
+    dplyr::select(-birth_number)
+}
+
+disposition.dataset <- function() {
+  disposition <- read.csv("data/disp.csv", sep = ";")
+  disposition <<- disposition %>%
+    group_by(account_id) %>% mutate(n_clients = n()) %>% ungroup() %>%
+    filter(type == "OWNER") %>%
+    dplyr::select(-type)
+}
+
+transacions.dataset <- function(train = TRUE) {
+  if (train) transactions <- read.csv("data/trans_train.csv", sep = ";")
+  else transactions <- read.csv("data/trans_test.csv", sep = ";")
+  # transactions <- read.csv("data/trans_train.csv", sep = ";")
+
+  transactions <<- transactions %>%
+    mutate(trans_year = date %/% 10000 + 1900) %>%
+    mutate(trans_month = date %% 10000 %/% 100) %>%
+    mutate(trans_day = date %% 100)
+}
+
+loan.dataset <- function(train = TRUE) {
+  if (train) loan <- read.csv("data/loan_train.csv", sep = ";")
+  else loan <- read.csv("data/loan_test.csv", sep = ";")
+  # loan <- read.csv("data/loan_train.csv", sep = ";")
+
+  loan <<- loan %>%
+    mutate(loan_year = date %/% 10000 + 1900) %>%
+    mutate(loan_month = date %% 10000 %/% 100) %>%
+    mutate(loan_day = date %% 100) %>%
+    dplyr::select(-date)
+}
+
+card.dataset <- function(train = TRUE) {
+  if (train) card <- read.csv("data/card_train.csv", sep = ";")
+  else card <- read.csv("data/card_test.csv", sep = ";")
+  # card <- read.csv("data/card_train.csv", sep = ";")
+
+  card <<- card %>%
+    mutate(card_year = issued %/% 10000 + 1900) %>%
+    mutate(card_month = issued %% 10000 %/% 100) %>%
+    mutate(card_day = issued %% 100) %>%
+    dplyr::select(-issued)
+}
+
+demograph.dataset <- function() {
+  demograph <- read.csv("data/district.csv", sep = ";")
+  demograph <<- demograph %>%
+    mutate(unemploymant.rate.95 = as.numeric(as.character(unemploymant.rate.95))) %>%
+    mutate(no..of.commited.crimes.95 = as.numeric(as.character(no..of.commited.crimes.95))) %>%
+    dplyr::select(-name)
+}
+
+
+### Merge Data Sets
+
+get.train.dataset <- function() {
+  prepare.datasets(train = TRUE)
+
+  data <- loan %>%
     left_join(account, by = "account_id", suffix = c('_loan', '_account')) %>%
     left_join(disposition, by = "account_id", suffix = c('', '_disp')) %>%
-    group_by(account_id) %>% mutate(n_accounts = n()) %>%
-    distinct(loan_id, account_id, .keep_all = TRUE) %>%
-    mutate(frequency = as.numeric(frequency)) %>%
-    dplyr::select(-type)
+    left_join(client, by = "client_id", suffix = c('', '_client')) %>%
+    mutate_if(is.factor, as.numeric) %>%
+    dplyr::select(-c(loan_id, account_id, district_id, disp_id, client_id))
 
   classes <- factor(data$status)
   predictors <- dplyr::select(data, -status)
@@ -30,17 +122,13 @@ get.train.dataset <- function() {
 }
 
 get.test.dataset <- function() {
-  loan.test <- read.csv("data/loan_test.csv", sep = ";")
-  account <- read.csv("data/account.csv", sep = ";")
-  disposition <- read.csv("data/disp.csv", sep = ";")
+  prepare.datasets(train = FALSE)
 
-  data <- loan.test %>%
+  data <- loan %>%
     left_join(account, by = "account_id", suffix = c('_loan', '_account')) %>%
     left_join(disposition, by = "account_id", suffix = c('', '_disp')) %>%
-    group_by(account_id) %>% mutate(n_accounts = n()) %>%
-    distinct(loan_id, account_id, .keep_all = TRUE) %>%
-    mutate(frequency = as.numeric(frequency)) %>%
-    dplyr::select(-type)
+    left_join(client, by = "client_id", suffix = c('', '_client')) %>%
+    dplyr::select(-c(account_id, district_id, disp_id, client_id))
 
   return (dplyr::select(data, -status))
 }
--- prediction.R
+++ prediction.R
@@ -10,7 +10,7 @@ library(AppliedPredictiveModeling)
 library(caret)
 library(MASS)
 library(e1071)
-
+library(ROCR) # ROC and AUC
 
 source("functions.R")
 source("datasets.R")
@@ -19,6 +19,8 @@ train.dataset <- get.train.dataset()
 predictors <- train.dataset$predictors
 classes <- train.dataset$classes
 test <- get.test.dataset()
+
+### K-Fold Cross Validation
 
 set.seed(1)
 cvSplits <- createFolds(classes, k = 10, returnTrain = TRUE)
@@ -33,37 +35,31 @@ for (i in 1:10) {
   testClasses <- classes[-trainFold]
 
   # lda (MASS)
-  ldaFit <- lda(x = trainPredictors, grouping = trainClasses)
-  ldaPredictions <- predict(ldaFit, newdata = testPredictors, type = "class")
-  table_mat <- table(testClasses, ldaPredictions$class)
-  accuracy <- sum(diag(table_mat)) / sum(table_mat)
-  # fulllda[[i]] <- list("model" = ldaFit, "table" = table_mat, "accuracy" = accuracy)
-  fulllda[[i]] <- accuracy
+  # ldaFit <- lda(x = trainPredictors, grouping = trainClasses)
+  # ldaPredictions <- predict(ldaFit, newdata = testPredictors, type = "class")
+  # table_mat <- table(testClasses, ldaPredictions$class)
+  # fulllda[[i]] <- sum(diag(table_mat)) / sum(table_mat)
 
   # knn3 (caret)
   knnFit <- knn3(x = trainPredictors, y = trainClasses, k = 5)
   knnPredictions <- predict(knnFit, newdata = testPredictors, type = "class")
   table_mat <- table(testClasses, knnPredictions)
-  accuracy <- sum(diag(table_mat)) / sum(table_mat)
-  # fullknn[[i]] <- list("model" = knnFit, "table" = table_mat, "accuracy" = accuracy)
-  fullknn[[i]] <- accuracy
+  fullknn[[i]] <- sum(diag(table_mat)) / sum(table_mat)
+
+  # Get the full data set
+  aux <- add_column(trainPredictors, status = trainClasses)
 
   # rpart (rpart)
-  aux <- add_column(trainPredictors, status = trainClasses)
   rpartFit <- rpart(status ~ ., data = aux)
   rpartPredictions <- predict(rpartFit, newdata = testPredictors, type = "class")
   table_mat <- table(testClasses, rpartPredictions)
-  accuracy <- sum(diag(table_mat)) / sum(table_mat)
-  # fullrpart[[i]] <- list("model" = rpartFit, "table" = table_mat, "accuracy" = accuracy)
-  fullrpart[[i]] <- accuracy
+  fullrpart[[i]] <- sum(diag(table_mat)) / sum(table_mat)
 
   # svm (e1071)
   svmFit <- svm(status ~ ., data = aux)
   svmPredictions <- predict(svmFit, newdata = testPredictors, type = "class")
   table_mat <- table(testClasses, svmPredictions)
-  accuracy <- sum(diag(table_mat)) / sum(table_mat)
-  # fullsvm[[i]] <- list("model" = svmFit, "table" = table_mat, "accuracy" = accuracy)
-  fullsvm[[i]] <- accuracy
+  fullsvm[[i]] <- sum(diag(table_mat)) / sum(table_mat)
 }
 
 fulllda
@@ -79,18 +75,37 @@ fullsvm
 mean(fullsvm)
 
 
-# Build the model with the full dataset
+### Build the model with the full dataset
+
 full_dataset <- add_column(predictors, status = classes)
+
+# K-Nearest Neighbors
+#model <- knn3(x = predictors, y = classes, k = 5)
+
+# CART
 model <- rpart(status ~ ., data = full_dataset)
 rpart.plot(model, extra = 106)
 
-# Check accuracy
-predictions <- predict(model, newdata = predictors, type = "class")
+
+### Check ROC, AUC and accuracy
+predictions <- predict(model, newdata = predictors, type = "prob")
+
+pred_ROCR <- prediction(predictions[,"-1"], classes)
+roc_ROCR <- performance(pred_ROCR, "tpr", "fpr")
+plot(roc_ROCR, main = "ROC curve")
+abline(0, 1, lty = 2)
+
+auc_ROCR <- performance(pred_ROCR, measure = "auc")
+auc_ROCR <- auc_ROCR@y.values[[1]]
+
+plot(performance(pred_ROCR, "acc"))
+
 table_mat <- table(classes, predictions)
 sum(diag(table_mat)) / sum(table_mat)
 
-# Predict values
-prediction <- predict(model, test, type = "prob")
-table(prediction)
 
-generate.prediction(loan.test, prediction[,"-1"], toCSV = TRUE)
+### Predict values
+prediction <- predict(model, test, type = "prob")
+prediction
+
+generate.prediction(test, prediction[,"-1"], toCSV = TRUE)
