a9e74e21341d3e9f4304ccf31f73280c2bda5819
g-batalhao-a <al.darkseid.2010@gmail.com>
2021-12-07 09:09:32 +0000

    Added cdv files and R

--- R/prediction.R
+++ R/prediction.R
@@ -83,7 +83,7 @@ generate.prediction(test.dataset, prediction[,"-1"], toCSV = TRUE)
 
 ## Clean prediction csv
 
-predictions <- read.csv("../prediction_testing_dataset.csv",sep = ";")
+predictions <- read.csv("../prediction_rf.csv",sep = ";")
 predictions <- dplyr::select(predictions, -c("status","prediction","positive"))
 predictions <- rename(predictions, "Id" = "loan_id") %>% rename("Predicted" = "negative")
 write.csv(predictions,"../prediction_clean.csv",sep = ",", row.names = FALSE)
